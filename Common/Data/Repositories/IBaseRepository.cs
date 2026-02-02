9f006cb44882b0db0c8c44f39345426c83baa463
Titouan Massé <titouanmasselaude@gmail.com>
2026-02-02 12:51:21 +0100

    :sparkles: Added base repository and it's interface :sparkles:

---
+++ Common/Data/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+using System.Linq.Expressions;
+
+namespace Common.Data.Repositories;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<TEntity> AddAsync(TEntity model, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity model, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TEntity model, CancellationToken cancellationToken = default);
+    Task SoftDeleteAsync(TEntity model, CancellationToken cancellationToken = default);
+    Task<TEntity?> FindAsync<TId>(TId id, CancellationToken cancellationToken = default) where TId : notnull;
+    Task<List<TEntity>> ListAsync(CancellationToken cancellationToken = default);
+    Task<List<TEntity>> ListAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+}
