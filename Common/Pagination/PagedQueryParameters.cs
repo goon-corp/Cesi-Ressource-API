544365feb88e7927bd37c4f89da885bcb50a7a80
Gosselin Noa <148050011+NoaGsl@users.noreply.github.com>
2026-02-03 11:44:16 +0100

    :sparkles: Merging pagination system :sparkles:
    Added pagination system to the API.

---
+++ Common/Pagination/PagedQueryParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace Ressource_API.Common.Pagination;
+
+public class PagedQueryParameters
+{
+    private const int MaxSize = 50;
+    private int _size = 10;
+    private int _page = 1;
+
+    public int page
+    {
+        get => _page;
+        set => _page = (value < 1) ? 1 : value;
+    }
+
+    // Ensure 'size' is within valid bounds (1 to MaxSize)
+    public int size
+    {
+        get => _size;
+        set => _size = (value < 1) ? 1 : (value > MaxSize ? MaxSize : value);
+    }
+}
