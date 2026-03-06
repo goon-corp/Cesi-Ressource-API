09cc87da8b20c83f88951d653d65ef51da5664e6
Titouan Massé <titouanmasselaude@gmail.com>
2026-03-06 11:40:16 +0100

    :bug: Modified dbcontext type in baseRepository

--- Features/Tags/Repositories/TagRepository.cs
+++ Features/Tags/Repositories/TagRepository.cs
@@ -9,10 +9,8 @@ namespace Ressource_API.Features.Tags.Repositories;
 
 public class TagRepository : BaseRepository<Tag>, ITagRepository
 {
-    private readonly ApplicationDbContext _context;
     public TagRepository(ApplicationDbContext context) : base(context)
     {
-        _context = context;
     }
     
     public async Task<PaginatedList<Tag>> PaginatedListAsync(
