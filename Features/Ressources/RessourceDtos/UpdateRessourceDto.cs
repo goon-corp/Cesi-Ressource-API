f2186e98bad3191b06eddbb376716648382ea1a6
Gosselin Noa <no.gosselin2006@gmail.com>
2026-03-28 21:36:29 +0100

    :fire: remove useless files :fire:

--- Features/Ressources/RessourceDtos/UpdateRessourceDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Ressource_API.Features.Ressources.RessourceDtos;
-
-public class UpdateRessourceDto
-{
-    // TODO: Add properties needed to update a Ressource
-    // Example:
-    // public string Name { get; set; } = string.Empty;
-    // public string Description { get; set; } = string.Empty;
-}
