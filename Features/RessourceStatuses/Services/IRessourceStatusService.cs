78ca1437fcb54be87b20f8a79b7d83d0fb667bcb
Gosselin Noa <no.gosselin2006@gmail.com>
2026-03-28 12:10:46 +0100

    feat: ressource status routes

--- Features/RessourceStatuses/Services/IRessourceStatusService.cs
+++ Features/RessourceStatuses/Services/IRessourceStatusService.cs
@@ -1,13 +1,9 @@
-using Ressource_API.Features.RessourceStatuses.Models;
+using Ressource_API.Common.ResultPattern;
 using Ressource_API.Features.RessourceStatuses.RessourceStatusDtos;
 
 namespace Ressource_API.Features.RessourceStatuses.Services;
 
 public interface IRessourceStatusService
 {
-    Task<IEnumerable<RessourceStatus>> GetAllRessourceStatussAsync(CancellationToken cancellationToken = default);
-    Task<RessourceStatus?> GetRessourceStatusByIdAsync(int id, CancellationToken cancellationToken = default);
-    Task<RessourceStatus> CreateRessourceStatusAsync(CreateRessourceStatusDto dto, CancellationToken cancellationToken = default);
-    Task<RessourceStatus?> UpdateRessourceStatusAsync(int id, UpdateRessourceStatusDto dto, CancellationToken cancellationToken = default);
-    Task<bool> DeleteRessourceStatusAsync(int id, CancellationToken cancellationToken = default);
+    Task<Result<IEnumerable<RessourceStatusInfoDto>>> GetAllAsync(CancellationToken cancellationToken = default);
 }
