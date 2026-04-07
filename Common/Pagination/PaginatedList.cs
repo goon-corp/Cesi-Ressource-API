3c84f7ea589c85bee5e74cdae5aed0492a8bef5d
Gosselin Noa <no.gosselin2006@gmail.com>
2026-04-07 11:05:38 +0200

    fix: update PaginatedList usage in RessourceService and TagService

--- Common/Pagination/PaginatedList.cs
+++ Common/Pagination/PaginatedList.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ressource_API.Common.Pagination;
 
-public class PaginatedList<T> : List<T>
+public class PaginatedList<T>
 {
     public List<T> Items { get; set; }
     public int PageIndex  { get; }
@@ -8,24 +8,18 @@ public class PaginatedList<T> : List<T>
     public int TotalCount { get; }
     public int TotalPages { get; }
 
-    public PaginatedList()
-    {
-        Items = new List<T>();
-    }
-    
     public PaginatedList(
         List<T> items,
         int pageIndex,
         int pageSize,
         int totalCount)
     {
+        Items = items;
         PageIndex = pageIndex;
         PageSize = pageSize;
         TotalCount = totalCount;
 
         TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
-
-        AddRange(items);
     }
 
     public bool HasPreviousPage => PageIndex > 1;
